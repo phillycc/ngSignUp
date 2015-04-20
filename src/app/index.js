a3fdaa3a473146e261c8d937625ed604cc6b5341
David Elmer <davidelmer.me@gmail.com>
2015-04-20 16:49:35 -0400

    Added ng-transclude

--- src/app/index.js
+++ src/app/index.js
@@ -1,3 +1,11 @@
 'use strict';
 
-angular.module('signup', ['ngAnimate']);
+var app = angular.module('signup', ['ngAnimate']);
+
+app.directive('optIn', function() {
+    return {
+        restrict: 'E',
+        transclude: true,
+        template: '<div ng-transclude class="brand-logo"></div>',
+    }
+})
